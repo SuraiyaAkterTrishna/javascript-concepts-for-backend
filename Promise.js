56149ddb38adf6bd45f7afa1826e36de17a48eff
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-08-22 20:32:49 +0600

    Promise concept

---
+++ Promise.js
@@ -0,0 +1,24 @@
+/**
+ * Promise have three states
+ * 1. Pending
+ * 2. resolved
+ * 3. rejected
+ */
+
+// Promise create 
+
+const myPromise = new Promise((resolve, reject) => {
+    const user = { id: 1 };
+    if(!user){
+        reject("Something went wrong!");
+    } else {
+        setTimeout( () => {
+            resolve("Successfully got the data");
+        }, 1000);
+    }
+});
+
+// Promise consume 
+myPromise
+    .then(res => console.log("found in then", res))
+    .catch(err => console.log("found in catch", err));
